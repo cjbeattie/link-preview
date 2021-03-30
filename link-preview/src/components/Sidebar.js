29f8f0a86fa77ca66ef648cfd50d296d6b73c103
cjbeattie <courtney.j.beattie+1@gmail.com>
2021-03-30 14:39:16 +0800

    Adding navlinks

--- link-preview/src/components/Sidebar.js
+++ link-preview/src/components/Sidebar.js
@@ -1,7 +1,17 @@
+import { Nav } from 'react-bootstrap';
+
 const Sidebar = () => {
     return (
         <>
             <p className="mt-4" style={{ color: 'white' }}>ONLOOP ADMIN</p>
+            <Nav defaultActiveKey="/home" className="flex-column">
+                <Nav.Link href="/home">Active</Nav.Link>
+                <Nav.Link eventKey="link-1">Link</Nav.Link>
+                <Nav.Link eventKey="link-2">Link</Nav.Link>
+                <Nav.Link eventKey="disabled" disabled>
+                    Disabled
+                </Nav.Link>
+            </Nav>
         </>
     )
 }
